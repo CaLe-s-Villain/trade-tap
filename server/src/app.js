22dd5d91cbf36cec72566f851633a455bbf87a73
FullSol <ceraines@student.fullsail.edu>
2025-03-21 21:01:53 -0400

    fix: apply CORS config inside app.js to allow frontend requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
-
 const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is working!");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from backend" });
 });
 
 module.exports = app;
